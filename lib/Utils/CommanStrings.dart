7aaae0b9f43bc37eabaabbfb1ba5d771b5fa2fc9
dinesh parmar <shashikha1000@gmail.com>
2021-09-24 16:31:30 +0530

    towfin url changes done

--- lib/Utils/CommanStrings.dart
+++ lib/Utils/CommanStrings.dart
@@ -2,8 +2,8 @@ import 'dart:convert';
 
 import 'package:shared_preferences/shared_preferences.dart';
 
-String BASE_URL = "https://roadrunnersclub.net/";
-String ASSET_URL = "http://lgemarrakech.com/";
+String BASE_URL = "https://www.towfin.com/";
+String ASSET_URL = "https://www.towfin.com/";
 String PAYSTACKKEY = "pk_live_993e4d50be323b3b8dbeeb84c6398ffa95e7adac";
 
 String getStatusUrl = "api/provider/trip?";
